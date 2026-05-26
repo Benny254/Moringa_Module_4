c80feed94b3afd05288ad9f322a5f61f58c21e33
Benny <asshimbenny01@gmail.com>
2026-05-26 11:05:10 +0300

    initial commit

---
+++ python-intro/for_loop/count_str.py
@@ -0,0 +1,8 @@
+word = "Programming"
+
+count = 0
+
+for letter in word:
+    count += 1
+
+print("Total Characters:", count)
---
+++ python-intro/for_loop/debug.py
@@ -0,0 +1,8 @@
+#wrong code 
+
+#for i in range(5)
+#print(i)
+
+#correct code 
+for i in range(5):
+    print(i)
