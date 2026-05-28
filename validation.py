a0a1cae3d5a314526bbcaf0494d52e424a3699b4
Benny <asshimbenny01@gmail.com>
2026-05-28 16:42:30 +0300

    fix validation for autograder

--- validation.py
+++ validation.py
@@ -1,26 +1,25 @@
-# task_manager/validation.py
-
 from datetime import datetime
 
 
 def validate_task_title(title):
     if not title.strip():
-        print("Error: Task title cannot be empty.")
-        return False
+        raise ValueError(...)
     return True
 
 
 def validate_task_description(description):
     if not description.strip():
-        print("Error: Task description cannot be empty.")
-        return False
+        raise ValueError(...)
+    if len(description) > 500:
+        raise ValueError(...)
     return True
 
 
 def validate_due_date(due_date):
+    if not due_date.strip():
+        raise ValueError(...)
     try:
         datetime.strptime(due_date, "%Y-%m-%d")
-        return True
     except ValueError:
-        print("Error: Due date must be in YYYY-MM-DD format.")
-        return False
+        raise ValueError(...)
+    return True
