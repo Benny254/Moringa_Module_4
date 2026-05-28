a0c93ef00551a27b6114a69f6e5682d974285afd
Benny <asshimbenny01@gmail.com>
2026-05-28 13:23:34 +0300

    initial commit

--- Task_Management_System/task_manager/validation.py
+++ Task_Management_System/task_manager/validation.py
@@ -1,26 +1,30 @@
-# task_manager/validation.py
-
 from datetime import datetime
 
 
 def validate_task_title(title):
-    if not title.strip():
-        print("Error: Task title cannot be empty.")
+
+    if len(title.strip()) == 0:
+        print("Invalid title.")
         return False
+
     return True
 
 
 def validate_task_description(description):
-    if not description.strip():
-        print("Error: Task description cannot be empty.")
+
+    if len(description.strip()) == 0:
+        print("Invalid description.")
         return False
+
     return True
 
 
 def validate_due_date(due_date):
+
     try:
         datetime.strptime(due_date, "%Y-%m-%d")
         return True
+
     except ValueError:
-        print("Error: Due date must be in YYYY-MM-DD format.")
+        print("Invalid due date format.")
         return False
