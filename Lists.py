b761103e2093d5165669c4167287d78f0cd2599f
Benny <asshimbenny01@gmail.com>
2026-06-22 09:47:49 +0300

    initial commit

---
+++ Lists.py
@@ -0,0 +1,21 @@
+# 1. Create a list
+fruits = ["apple", "banana", "cherry", "date"]
+
+# 2. Print an element by index
+print(fruits[1])  # banana
+
+# 3. Add an item to the end of the list
+fruits.append("orange")
+
+# 4. Remove an item from the list
+fruits.remove("cherry")
+
+# 5. Iterate over the list
+for fruit in fruits:
+    print(fruit)
+
+# 6. Reverse the list
+fruits.reverse()
+
+# Print reversed list
+print("Reversed:", fruits)
